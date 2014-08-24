31c73a957f1c45cef49bed872b789b6fd777c08d
Amir-gihub <Amir-gihub@users.noreply.github.com>
2014-08-25 01:31:40 +0200

    Create run_analysis.R

---
+++ run_analysis.R
@@ -0,0 +1,66 @@
+# Step1
+MainData <- read.table("./data/train/X_train.txt")
+dim(MainData) 
+head(MainData)
+MainLabel <- read.table("./data/train/y_train.txt")
+table(MainLabel)
+MainSubject <- read.table("./data/train/subject_train.txt")
+testData <- read.table("./data/test/X_test.txt")
+dim(testData) 
+testLabel <- read.table("./data/test/y_test.txt") 
+table(testLabel) 
+testSubject <- read.table("./data/test/subject_test.txt")
+joinData <- rbind(trainData, testData)
+dim(joinData) 
+joinLabel <- rbind(trainLabel, testLabel)
+dim(joinLabel)
+joinSubject <- rbind(trainSubject, testSubject)
+dim(joinSubject)
+
+# Step2 
+features <- read.table("./data/features.txt")
+dim(features)  
+meanStdIndices <- grep("mean\\(\\)|std\\(\\)", features[, 2])
+length(meanStdIndices) 
+joinedData <- joinData[, meanStdIndices]
+dim(joinedData) 
+names(joinedData) <- gsub("\\(\\)", "", features[meanStdIndices, 2]) 
+names(joinedData) <- gsub("mean", "Mean", names(joinedData)) 
+names(joinedData) <- gsub("std", "Std", names(joinedData)) 
+names(joinedData) <- gsub("-", "", names(joinedData))  
+
+# Step3. 
+activity <- read.table("./data/activity_labels.txt")
+activity[, 2] <- tolower(gsub("_", "", activity[, 2]))
+substr(activity[2, 2], 8, 8) <- toupper(substr(activity[2, 2], 8, 8))
+substr(activity[3, 2], 8, 8) <- toupper(substr(activity[3, 2], 8, 8))
+activityLabel <- activity[joinLabel[, 1], 2]
+joinLabel[, 1] <- activityLabel
+names(joinLabel) <- "activity"
+
+# Step4. 
+names(joinSubject) <- "subject"
+cleanedData <- cbind(joinSubject, joinLabel, joinedData)
+dim(cleanedData) 
+write.table(cleanedData, "merged_data.txt") 
+
+# Step5.  
+subjectLen <- length(table(joinSubject)) 
+activityLen <- dim(activity)[1] 
+columnLen <- dim(cleanedData)[2]
+result <- matrix(NA, nrow=subjectLen*activityLen, ncol=columnLen) 
+result <- as.data.frame(result)
+colnames(result) <- colnames(cleanedData)
+row <- 1
+for(i in 1:subjectLen) {
+    for(j in 1:activityLen) {
+        result[row, 1] <- sort(unique(joinSubject)[, 1])[i]
+        result[row, 2] <- activity[j, 2]
+        bool1 <- i == cleanedData$subject
+        bool2 <- activity[j, 2] == cleanedData$activity
+        result[row, 3:columnLen] <- colMeans(cleanedData[bool1&bool2, 3:columnLen])
+        row <- row + 1
+    }
+}
+head(result)
+write.table(result, "NEW_dataset.txt") 
